bd7d0a80561bb3fdf44af2f312a3221feea483ad
Joris Vos <jorisvos037@gmail.com>
2019-01-22 14:36:08 +0100

    Removed 'Init' commit and added database

--- src/inc/functions.php
+++
@@ -1,4 +0,0 @@
-<?php
-    function IsNullOrEmptyString($str) { return (strlen($str) < 1 || empty($str)); }
-    function IsNotNullOrEmptyString($str) { return (strlen($str) > 0 || !empty($str)); }
-?>
